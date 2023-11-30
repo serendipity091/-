a8954beda84650c4c91247df8215d440e8cb369f
serendipity091 <150596852+serendipity091@users.noreply.github.com>
2023-11-30 09:30:33 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=27382229&pid=ccc24be8120691e092c897bb9fd53f80&tid=f43170c1b151bcc9ef8d7e9a4abe13fe",
+    "https://www.maimemo.com/share/page?uid=27382229&pid=f3445616d0371313a3cc6be894642adc&tid=e917a1c1b0e4522a0eca05c78512cc78",
 ]
   /**
    * 生成随机数字
